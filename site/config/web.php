82889328fb1be4a015ce579155b54f008c4c0e30
Mester Peter <mester@localhost.localdomain>
2022-03-25 13:56:11 +0100

    added say and country.

--- site/config/web.php
+++ site/config/web.php
@@ -62,13 +62,15 @@ if (YII_ENV_DEV) {
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        // 'allowedIPs' => ['localhost:81', '::1'],
+        'allowedIPs' => ['*'],
     ];
 }
 
